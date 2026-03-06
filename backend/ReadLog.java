d69bb59a7d3307ac1a09c00d2e429333dec2795d
wiiiii123 <hong95402@st.vimaru.edu.vn>
2026-03-06 08:28:52 +0700

    chore: remove scratch files and tracked upload artifacts

--- backend/ReadLog.java
+++
@@ -1,9 +0,0 @@
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-public class ReadLog {
-    public static void main(String[] args) throws Exception {
-        Files.lines(Paths.get("build_error.log")).forEach(System.out::println);
-    }
-}
