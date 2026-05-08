24d729ba6ef2ecc4d09b96d9da48d7e6976624f8
wiiiii123 <hong95402@st.vimaru.edu.vn>
2026-05-08 20:32:37 +0700

    Polish public mobile course UX

--- fe/src/app/features/auth/forgot-password/forgot-password.component.ts
+++ fe/src/app/features/auth/forgot-password/forgot-password.component.ts
@@ -300,8 +300,8 @@ type ForgotPasswordForm = {
           <a routerLink="/" class="auth-brand">
             <img src="/icons/logo-master.png" alt="LMS Maritime" class="h-11 w-11 rounded-2xl shadow-sm">
             <div>
-              <p class="auth-brand-kicker">LMS Maritime</p>
-              <p class="auth-brand-title">HoHoLiHu</p>
+              <p class="auth-brand-kicker">HoLiLiHu Online</p>
+              <p class="auth-brand-title">LMS Maritime</p>
             </div>
           </a>
 
